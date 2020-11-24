b23215d186ce5b99400b71451d00fae9686cfa42
Marty Warfel <marthawarfel@gmail.com>
2020-11-24 19:58:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,11 +27,11 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-    array
+    array.index(index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array.
 end
 
 def retrieve_last_element_from_array(array)
